d99879bde2656faa52d4ec57437089e5e3d2d4c9
KHELIFA Yassine <yacinemosta910@gmail.com>
2026-09-22 21:33:38 +0200

    Prepare frontend for production API

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     Accept: "application/json",
   },
 });
-
